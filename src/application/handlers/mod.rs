6ed5992d8d314b3b3b05b453e62cbe2ff0a5d016
ax <axiomorient.dev@gmail.com>
2026-09-01 13:04:32 +0900

    wire typed application handlers

--- src/application/handlers/mod.rs
+++ src/application/handlers/mod.rs
@@ -1,5 +1,11 @@
 mod asset;
+mod motion;
 mod request;
+mod sprite;
+mod vector;
 
 pub(super) use asset::{convert, inspect, upscale};
+pub(super) use motion::{build as motion_build, scaffold as motion_scaffold};
 pub(super) use request::{chroma_plan, edit, export_psd};
+pub(super) use sprite::{bundle, normalize};
+pub(super) use vector::{analyze as vector_analyze, compile as vector_compile};
