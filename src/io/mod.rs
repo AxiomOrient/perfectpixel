f992b13d2753a2cbdeb0f95f2b25859efc8612c2
ax <axiomorient.dev@gmail.com>
2026-09-01 14:47:25 +0900

    io: expose file publication preconditions

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -10,7 +10,9 @@ pub use artifact_set::{
     reject_blocked_managed_parents, ArtifactSetConditionPhase, AtomicArtifactSetEntry,
     AtomicArtifactSetOwnedEntry, AtomicArtifactSetOwnedPlan, AtomicArtifactSetWriter,
 };
-pub use atomic::{AtomicDirectoryEntry, AtomicDirectoryWriter, AtomicFileWriter};
+pub use atomic::{
+    AtomicDirectoryEntry, AtomicDirectoryWriter, AtomicFileWriter, FilePrecondition,
+};
 pub use codec::*;
 #[doc(hidden)]
 pub use parallel::{parallel_map, parallel_map_owned};
